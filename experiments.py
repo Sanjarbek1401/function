3e9f04c02d77e527133fc7af7e4a6b888a807219
Sanjarbek <sanjarbahodirov9901@gmail.com>
2024-04-19 16:00:58 +0300

    fqwfq3fqegwqrfq

---
+++ experiments.py
@@ -0,0 +1,28 @@
+# istalgan sonni ildizini chiqarish
+"""
+def sonini_ildizini_chiqarib_ber(son):
+    ildiz = son ** 0.5
+    return ildiz
+
+son = int(input("Istalgan sonni kiriting: "))
+print(f"{son} ning ildizi: {sonini_ildizini_chiqarib_ber(son)}")
+"""
+# random 
+"""
+import random
+print(random.randrange(1,10))
+"""
+"""yosh = 27
+yildagi_kunlar = 365
+sana = 3.25 * yildagi_kunlar
+natija = yosh * sana
+print(f"{yosh} yoshdagi odam {sana} kun yashab {natija} kun yashab.")
+"""
+
+umumiy_tolov = 15.75
+taom_soni = 3
+har_bir_taom_tolovi = umumiy_tolov / taom_soni
+print(f"Har bir taom uchun $ {har_bir_taom_tolovi} to'lashing kerak.")
+print(f"Jami to'lov: $ {umumiy_tolov}")
+
+
