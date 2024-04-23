13fe17a85aa61afedf652c7de3674fd6410912b6
Sanjarbek1401 <sanjarbahodirov9901@gmail.com>
2024-04-23 14:44:13 +0300

    I have changed experiments.py

--- experiments.py
+++ experiments.py
@@ -20,8 +20,18 @@ print(f"{yosh} yoshdagi odam {sana} kun yashab {natija} kun yashab.")
 
 print("Hello world")
 
-
-
+import re
+def clean_text(text):
+    # remove extra whitespace
+     text = re.sub(' +', ' ', text)
+     # remove special characters
+     text = re.sub('[^A-Za-z0-9 ]+', '', text)
+     #convert to lowercase
+     text = text.lower()
+     return text
+input_text = input("Enter text:")
+cleaned_text = clean_text(input_text)
+print("Cleaned_text:", cleaned_text)
 
 
 
