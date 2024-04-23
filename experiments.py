fcb15ec5522e1ccc3a21414a31543001c245e753
Sanjarbek1401 <sanjarbahodirov9901@gmail.com>
2024-04-23 14:30:09 +0300

    it has been changed

--- experiments.py
+++ experiments.py
@@ -12,17 +12,14 @@ print(f"{son} ning ildizi: {sonini_ildizini_chiqarib_ber(son)}")
 import random
 print(random.randrange(1,10))
 """
-"""yosh = 27
+yosh = 27
 yildagi_kunlar = 365
 sana = 3.25 * yildagi_kunlar
 natija = yosh * sana
 print(f"{yosh} yoshdagi odam {sana} kun yashab {natija} kun yashab.")
-"""
 
-umumiy_tolov = 15.75
-taom_soni = 3
-har_bir_taom_tolovi = umumiy_tolov / taom_soni
-print(f"Har bir taom uchun $ {har_bir_taom_tolovi} to'lashing kerak.")
-print(f"Jami to'lov: $ {umumiy_tolov}")
+
+
+
 
 
